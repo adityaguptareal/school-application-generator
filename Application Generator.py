7d770d552666c65d53daf5a16b6938e02bb4bdee
Aditya Kumar Gupta <78353347+adityaguptareal@users.noreply.github.com>
2022-11-02 17:45:33 +0530

    Create a School Application Generator
    Create a basic school application generator software using python

---
+++ Application Generator.py
@@ -0,0 +1,25 @@
+Appplication='''To,
+
+The Principal,
+
+[Name of the school],
+
+[Address],
+
+[Date]
+
+Subject: Application for leave
+
+Sir/Madam,
+
+I am a student of class [Your class and section] in your school. Actually, I have to go with my mother to visit my grandmother at her place due to some urgent piece of work. Therefore, it is not possible for me to attend school tomorrow. Therefore, kindly grant me leave for one day. I shall be really thankful to you for this kind act.
+
+Thanking you,
+
+Yours obediently,
+
+[Your Name]
+
+[Class and section]
+
+[Roll No.]'''
